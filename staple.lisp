c4a764b624e503d3519ccc19f84db739ddbc2e49
Shinmera <shinmera@tymoon.eu>
2016-07-09 13:26:25 +0200

    Staple helper

---
+++ staple.lisp
@@ -0,0 +1,34 @@
+(ql:quickload '(staple for))
+(in-package #:cl-user)
+
+(defclass symb-binding (staple:symb-function)
+  ())
+
+(defmethod staple:symb-function ((symb symb-binding))
+  (for:binding (staple:symb-symbol symb)))
+
+(defmethod staple:symb-documentation ((symb symb-binding))
+  (documentation (staple:symb-symbol symb) 'for:binding))
+
+(defmethod staple:symb-type-order ((symb (eql 'symb-binding)))
+  (1+ (staple:symb-type-order 'staple:symb-function)))
+
+(defclass symb-clause (staple:symb-function)
+  ())
+
+(defmethod staple:symb-function ((symb symb-clause))
+  (for:clause (staple:symb-symbol symb)))
+
+(defmethod staple:symb-documentation ((symb symb-clause))
+  (documentation (staple:symb-symbol symb) 'for:clause))
+
+(defmethod staple:symb-type-order ((symb (eql 'symb-clause)))
+  (1+ (staple:symb-type-order 'symb-binding)))
+
+(staple:define-simple-converter symb-binding for:binding)
+(staple:define-simple-converter symb-clause for:clause)
+
+(defun staple ()
+  (staple:generate :for
+                   :packages '(:for :for-iterator)
+                   :if-exists :supersede))
